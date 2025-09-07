33bdcdddb1f8727affc64e8d18a9a4e04f700af2
Sutanu1234 <sutanubera82@gmail.com>
2025-09-07 15:24:53 +0530

    some change

--- src/pages/home/home-page.jsx
+++ src/pages/home/home-page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import LandingPage from './components/middle-page'
-import ShopNow from './components/shop-now'
+// import ShopNow from './components/shop-now'
 import AboutUs from './components/about-us'
 import Footer from './components/footer-sec'
 
@@ -8,7 +8,7 @@ export default function HomePage() {
   return (
     <>
       <LandingPage />
-      <ShopNow />
+      {/* <ShopNow /> */}
       <AboutUs />
       <Footer />
     </>
